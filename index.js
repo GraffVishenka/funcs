988c6573512141d915d6ecb19702e150d76f0c8d
GraffVishenka <65270494+GraffVishenka@users.noreply.github.com>
2024-03-15 22:33:55 +0500

    func3

--- index.js
+++ index.js
@@ -1,97 +1,51 @@
-// 1 Задание
+// Задание 1
 
-const vowelsCount = (str) => {
-  let count = 0;
-  const vowelsChars = [
-    "a",
-    "e",
-    "y",
-    "u",
-    "i",
-    "o",
-    "е",
-    "а",
-    "о",
-    "э",
-    "я",
-    "и",
-    "ю",
-  ];
+function createPhoneNumber(numbers) {
+  let format = "(xxx) xxx-xxxx";
 
-  for (let char of str.substr(0, 14).toLowerCase()) {
-    if (vowelsChars.includes(char)) {
-      count++;
+  for (let i = 0; i < numbers.length; i++) {
+    format = format.replace("x", numbers[i]);
+  }
+
+  return format;
+}
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+// Задание 2
+
+function uniqueInOrder(x) {
+  const result = [];
+  const input = Array.isArray(x) ? x : x.split("");
+
+  for (let i = 0; i < input.length; ++i) {
+    if (input[i] == input[i + 1]) continue;
+    result.push(input[i]);
+  }
+
+  return result;
+}
+
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+console.log(uniqueInOrder("ABBCcAD"));
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
+
+// Задание 3
+
+function XO(str) {
+  let x = 0;
+  let o = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() === "x") {
+      x++;
+    } else if (str[i].toLowerCase() === "o") {
+      o++;
     }
   }
-  return count;
-};
-
-// 2 Задание
-
-const swapA = (str) => {
-  let newStr = "";
-  const swapChars = ["a", "A", "а", "А"];
-  for (let i = 0; i < 15; i++) {
-    if (swapChars.includes(str[i])) {
-      newStr += "#";
-    } else {
-      newStr += str[i];
-    }
-  }
-  return newStr;
-};
-
-// 3 Задание
-
-const firstLetterIsUppper = (str) => {
-  return str
-    .substr(0, 14)
-    .toLowerCase()
-    .split(" ")
-    .map((word) => {
-      return word[0].toUpperCase() + word.substr(1);
-    })
-    .join(" ");
-};
-
-// 4 Задание
-
-const sum = (nums) => {
-  if (nums.length === 0) return 0;
-  let sum = 0;
-  for (let num of nums) {
-    sum += num;
-  }
-  return sum;
-};
-
-// 5 Задание
-
-const evenCount = () => {
-  for (let i = 0; i <= 20; i++) {
-    if (i % 2 === 0 && i !== 0) {
-      console.log(i);
-    }
-  }
-};
-
-// 6 Задание
-
-const getLength = (source) => source.map((str) => str.length);
-
-// 7 Задание
-
-const removeDups = (arr) => {
-  let unique = [],
-    hashes = {};
-  for (let i = 0; i < arr.length; i++) {
-    let currentHash = JSON.stringify(!!arr[i] ? arr[i] : "" + arr[i]);
-    if (!hashes[currentHash]) {
-      unique.push(arr[i]);
-      hashes[currentHash] = true;
-    }
-  }
-  return unique;
-};
-
-console.log(removeDups([1, 1, 2, 3, 4, 66, 6, 6]));
+  return x === o;
+}
+console.log(XO("ooxx"));
+console.log(XO("xooxx"));
+console.log(XO("ooxXm"));
+console.log(XO("zpzpzpp"));
+console.log(XO("zzoo"));
