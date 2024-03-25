9552c6a369f962339a55421c7375471943f82d9e
GraffVishenka <65270494+GraffVishenka@users.noreply.github.com>
2024-03-25 08:22:16 +0500

    a

--- index.js
+++ index.js
@@ -1,51 +1,44 @@
 // Задание 1
 
-const createPhoneNumber = (numbers) => {
-  let format = "(xxx) xxx-xxxx";
-
-  for (let i = 0; i < numbers.length; i++) {
-    format = format.replace("x", numbers[i]);
+function caesarCipher(str, shift) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+  let charCode = str.charCodeAt(i);
+  if (charCode >= 65 && charCode <= 90) {
+  result += String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
+  } else if (charCode >= 97 && charCode <= 122) {
+  result += String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
+  } else {
+  result += str.charAt(i);
   }
-
-  return format;
-}
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
-
-// Задание 2
-
-const uniqueInOrder = (x) => {
-  const result = [];
-  const input = Array.isArray(x) ? x : x.split("");
-
-  for (let i = 0; i < input.length; ++i) {
-    if (input[i] == input[i + 1]) continue;
-    result.push(input[i]);
   }
-
   return result;
-}
+  }
+ 
+  let encrypted = caesarCipher('Hello, world!', 3);
+  console.log(encrypted);
 
-console.log(uniqueInOrder("AAAABBBCCDAABBB"));
-console.log(uniqueInOrder("ABBCcAD"));
-console.log(uniqueInOrder([1, 2, 2, 3, 3]));
 
 // Задание 3
 
-const XO = (str) => {
-  let x = 0;
-  let o = 0;
+const products = [
+  { id: 1, name: "Лаптоп", category: "Электроника" },
+  { id: 2, name: "Книга", category: "Литература" },
+  { id: 3, name: "Фотоаппарат", category: "Электроника" },
+  { id: 4, name: "Сумка", category: "Мода" },
+  { id: 5, name: "Книга", category: "Литература" },
+  { id: 6, name: "Таблетки", category: "Лекарство" },
+];
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].toLowerCase() === "x") {
-      x++;
-    } else if (str[i].toLowerCase() === "o") {
-      o++;
-    }
-  }
-  return x === o;
-}
-console.log(XO("ooxx"));
-console.log(XO("xooxx"));
-console.log(XO("ooxXm"));
-console.log(XO("zpzpzpp"));
-console.log(XO("zzoo"));
+const groupBy = (key) => {
+  return (group = (array) => {
+    return array.reduce((acc, obj) => {
+      const property = obj[key];
+      acc[property] = acc[property] || [];
+      acc[property].push(obj);
+      return acc;
+    }, {});
+  });
+};
+
+const filterProducts = groupBy("category");
